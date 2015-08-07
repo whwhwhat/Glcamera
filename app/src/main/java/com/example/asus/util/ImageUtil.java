0b5ef6bf20f326c9545137cb59574e494d1948de
whwhwhat <whwhwhat@gmail.com>
2015-08-07 11:29:28 +0800

    alpha

---
+++ app/src/main/java/com/example/asus/util/ImageUtil.java
@@ -0,0 +1,22 @@
+package com.example.asus.util;
+
+/**
+ * Created by asus on 2015/8/7 0007.
+ */
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+
+public class ImageUtil {
+    /**
+     * 旋转Bitmap
+     * @param b
+     * @param rotateDegree
+     * @return
+     */
+    public static Bitmap getRotateBitmap(Bitmap b, float rotateDegree){
+        Matrix matrix = new Matrix();
+        matrix.postRotate((float)rotateDegree);
+        Bitmap rotaBitmap = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(), matrix, false);
+        return rotaBitmap;
+    }
+}
